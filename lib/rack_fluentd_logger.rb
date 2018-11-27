864c1c4a7e735337d964fba5f35215a1b27b4034
Mikhail Slyusarev <mikhails@corporatetools.com>
2018-11-27 07:12:40 -0800

    adding concurrent-ruby

--- lib/rack_fluentd_logger.rb
+++ lib/rack_fluentd_logger.rb
@@ -1,28 +1,39 @@
 # frozen_string_literal: true
 
+require 'fluentd-logger'
+require 'concurrent-ruby'
+
 class RackFluentdLogger
   VERSION = '0.1.0'
+  JSON_PARSER = ->(str) { JSON.parse(str) }
 
   class << self
-    attr_reader :logger, :extra
+    attr_reader :logger, :json_parser
 
-    def configure(name: nil, host: nil, port: nil, extra: nil)
+    def configure(name: nil, host: nil, port: nil, json_parser: JSON_PARSER)
       @logger = Fluent::Logger::FluentLogger.new(
-        name || ENV['FLUENTD_NAME'],
-        host: host || ENV['FLUENTD_HOST'],
-        port: port || ENV['FLUENTD_PORT']
+        name || env.name,
+        host: host || env.host,
+        port: port || env.port
       )
 
-      @extra = extra
+      @json_parser = json_parser
+    end
+
+    def env
+      OpenStruct.new(
+        name: ENV['FLUENTD_NAME'],
+        host: ENV['FLUENTD_HOST'],
+        port: (ENV['FLUENTD_PORT'] || 24_224).to_i
+      )
     end
   end
 
   def initialize(app)
     @app = app
+    @executer = Concurrent::SingleThreadExecutor.new
 
-    if self.class.logger.nil?
-      self.class.configure
-    end
+    self.class.configure if self.class.logger.nil?
   end
 
   def call(env)
@@ -36,11 +47,22 @@ class RackFluentdLogger
   private
 
   def log_request(env, response)
-    self.class.logger.post(
-      'rack-traffic-log',
-      env: env,
-      response: response,
-      extra: self.class.extra&.call
-    )
+    @executer.post do
+      self.class.logger.post(
+        'rack-traffic-log',
+        env: env,
+        **format_response(response)
+      )
+    end
+  end
+
+  def format_response(response)
+    code, body, headers = response
+
+    if headers['Content-Type'] == 'application/json'
+      body = self.class.json_parser.call(body)
+    end
+
+    { code: code, body: body, headers: headers }
   end
 end
