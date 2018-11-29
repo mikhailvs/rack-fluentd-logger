5bed9b3e7b38a0af55fe81ba7f7097f6ebbcb12e
Mikhail Slyusarev <mikhails@corporatetools.com>
2018-11-29 00:00:00 +0000

    reorganizing a bit

---
+++ lib/rack/fluentd_logger_version.rb
@@ -0,0 +1,7 @@
+# frozen_string_literal: true
+
+module Rack
+  class FluentdLogger
+    VERSION = '0.1.0'
+  end
+end
--- lib/version.rb
+++
@@ -1,5 +0,0 @@
-# frozen_string_literal: true
-
-class RackFluentdLogger
-  VERSION = '0.1.0'
-end
